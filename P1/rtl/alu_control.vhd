2b6350ebc6ed723c5d353bfa7768df95448c9748
Lobo <carlos.gomezlobo99@gmail.com>
2019-09-26 23:01:01 +0200

    Primer push

---
+++ P1/rtl/alu_control.vhd
@@ -0,0 +1,74 @@
+--------------------------------------------------------------------------------
+-- Bloque de control para la ALU. Arq0 2019-2020.
+--
+-- (INCLUIR AQUI LA INFORMACION SOBRE LOS AUTORES, Quitar este mensaje)
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity alu_control is
+   port (
+      -- Entradas:
+      ALUOp  : in std_logic_vector (2 downto 0); -- Codigo de control desde la unidad de control
+      Funct  : in std_logic_vector (5 downto 0); -- Campo "funct" de la instruccion
+      -- Salida de control para la ALU:
+      ALUControl : out std_logic_vector (3 downto 0) -- Define operacion a ejecutar por la ALU
+   );
+end alu_control;
+
+architecture rtl of alu_control is
+
+   constant ALU_OR   : t_aluControl := "0111";   
+   constant ALU_NOT  : t_aluControl := "0101";
+   constant ALU_AND  : t_aluControl := "0100";
+   constant ALU_SUB  : t_aluControl := "0001";
+   constant ALU_ADD  : t_aluControl := "0000";
+   constant ALU_SLT  : t_aluControl := "1010";
+   constant ALU_S16  : t_aluControl := "1101";
+
+   constant FUNCT_OR   : t_aluControl := "100101";
+   constant FUNCT_XOR  : t_aluControl := "100110";
+   constant FUNCT_AND  : t_aluControl := "100100";
+   constant FUNCT_SUB  : t_aluControl := "100010";
+   constant FUNCT_ADD  : t_aluControl := "100000";
+   constant FUNCT_NOP  : t_aluControl := "000000";
+
+begin
+	
+   process(Funct, ALUOp)
+
+   begin
+
+      if ALUOp == "000" then
+         case Funct is
+            when FUNCT_OR => ALUControl <= ALU_OR;
+            when FUNCT_XOR => ALUControl <= ALU_XOR;
+            when FUNCT_AND => ALUControl <= ALU_AND;
+            when FUNCT_SUB => ALUControl <= ALU_SUB;
+            when FUNCT_ADD | FUNCT_NOP => ALUControl <= ALU_ADD;
+	    when others => (others <= '0');
+	 end case;
+
+      elsif ALUOp == "001" then
+         ALUControl <= ALU_NOT;
+      
+      elsif ALUOp == "010" then
+         ALUControl <= ALU_SLT;
+
+      elsif ALUOp == "011" then
+         ALUControl <= ALU_S16;
+
+      elsif ALUOp == "100" then
+         ALUControl <= ALU_SUB;
+
+      elsif ALUOp == "101" then
+         ALUControl <= ALU_ADD
+       
+      end if;  
+
+   end process;
+
+end architecture;
---
+++ P1/rtl/processor.vhd
@@ -0,0 +1,148 @@
+--------------------------------------------------------------------------------
+-- Procesador MIPS con pipeline curso Arquitectura 2019-2020
+--
+-- (INCLUIR AQUI LA INFORMACION SOBRE LOS AUTORES)
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity processor is
+   port(
+      Clk         : in  std_logic; -- Reloj activo en flanco subida
+      Reset       : in  std_logic; -- Reset asincrono activo nivel alto
+      -- Instruction memory
+      IAddr      : out std_logic_vector(31 downto 0); -- Direccion Instr
+      IDataIn    : in std_logic_vector(31 downto 0); -- Instruccion leida
+      -- Data memory
+      DAddr      : out std_logic_vector(31 downto 0); -- Direccion
+      DRdEn      : out std_logic;                     -- Habilitacion lectura
+      DWrEn      : out std_logic;                     -- Habilitacion escritura
+      DDataOut   : out std_logic_vector(31 downto 0); -- Dato escrito
+      DDataIn    : in  std_logic_vector(31 downto 0)  -- Dato leido
+   );
+end processor;
+
+architecture rtl of processor is 
+
+--General signals
+signal immEx : std_logic_vector(31 downto 0);
+immEx(15 downto 0) <= IDataIn(15 downto 0);
+imEx(31 downto 16) <= (others => IDataIn(15));
+
+-- Control Unit Signals
+signal Branch : std_logic;
+signal MemToReg : std_logic;
+signal MemWrite : std_logic;
+signal MemRead  : std_logic;
+signal ALUSrc : std_logic;
+signal ALUOp  : std_logic_vector (2 downto 0);
+signal RegWrite : std_logic;
+signal RegDst   : std_logic;
+signal A3s : std_logic_vector(4 downto 0);
+signal Wd3s : std_logic_vector(31 downto 0);
+
+--Alu signals
+signal Result : std_logic_vector (31 downto 0);
+signal OpA : std_logic_vector (31 downto 0);
+signal OpB : std_logic_vector (31 downto 0);
+signal ALUControl : std_logic_vector (3 downto 0)
+
+--Reg-bank signals
+signal Rd2s : std_logic_vector(31 downto 0);
+
+component control_unit is
+   port (
+      -- Entrada = codigo de operacion en la instruccion:
+      OpCode  : in  std_logic_vector (5 downto 0);
+      -- Seniales para el PC
+      Branch : out  std_logic; -- 1 = Ejecutandose instruccion branch
+      -- Seniales relativas a la memoria
+      MemToReg : out  std_logic; -- 1 = Escribir en registro la salida de la mem.
+      MemWrite : out  std_logic; -- Escribir la memoria
+      MemRead  : out  std_logic; -- Leer la memoria
+      -- Seniales para la ALU
+      ALUSrc : out  std_logic;                     -- 0 = oper.B es registro, 1 = es valor inm.
+      ALUOp  : out  std_logic_vector (2 downto 0); -- Tipo operacion para control de la ALU
+      -- Seniales para el GPR
+      RegWrite : out  std_logic; -- 1=Escribir registro
+      RegDst   : out  std_logic  -- 0=Reg. destino es rt, 1=rd
+      );
+end component;
+
+component reg_bank is
+
+   port (
+      Clk   : in std_logic; -- Reloj activo en flanco de subida
+      Reset : in std_logic; -- Reset asíncrono a nivel alto
+      A1    : in std_logic_vector(4 downto 0);   -- Dirección para el puerto Rd1
+      Rd1   : out std_logic_vector(31 downto 0); -- Dato del puerto Rd1
+      A2    : in std_logic_vector(4 downto 0);   -- Dirección para el puerto Rd2
+      Rd2   : out std_logic_vector(31 downto 0); -- Dato del puerto Rd2
+      A3    : in std_logic_vector(4 downto 0);   -- Dirección para el puerto Wd3
+      Wd3   : in std_logic_vector(31 downto 0);  -- Dato de entrada Wd3
+      We3   : in std_logic -- Habilitación de la escritura de Wd3
+   ); 
+
+end component;
+
+component alu_control is
+   port (
+      -- Entradas:
+      ALUOp  : in std_logic_vector (2 downto 0); -- Codigo de control desde la unidad de control
+      Funct  : in std_logic_vector (5 downto 0); -- Campo "funct" de la instruccion
+      -- Salida de control para la ALU:
+      ALUControl : out std_logic_vector (3 downto 0) -- Define operacion a ejecutar por la ALU
+   );
+end component;
+
+begin
+
+   CU : control_unit port map (
+      OpCode => IDataIn,
+      Branch => Branch,
+      MemToReg => MemToReg,
+      MemWrite => MemWrite,
+      MemRead => MemRead,
+      ALUSrc => ALUSrc,
+      ALUOp => ALUOp,
+      RegWrite => RegWrite,
+      RegDst => RegDst
+      );
+
+   RB : reg_bank port map (
+      Clk => Clk,
+      Reset => Reset,
+      A1 => IDataIn(25 downto 21),
+      A2 => IDataIn(20 downto 16),
+      A3 => A3s,
+      Wd3 => Wd3s,
+      We3 => RegWrite,
+      Rd1 => OpA,
+      Rd2 => Rd2s
+      );
+
+   ALU_C : alu_control port map (
+      Funct => IDataIn(5 downto 0),
+      ALUOp => ALUOp,
+      ALUControl => ALUControl
+      );
+
+   case RegDst is
+      when '0' => A3s <= IDataIn(20 downto 16)
+      when '1' => A3s <= IDataIn(15 downto 11)
+   end case;
+
+   case MemToReg is
+      when '0' => Wd3s <= Result
+      when '1' => Wd3s <= DDataOut
+   end case;
+
+   case ALUSrc is
+      when '0' => OpB <= Rd2s
+      when '1' => OpB <= immEx
+   end case;
+ 
+end architecture;
