432a135ad2ff2b76013ce961a200dfe285267acb
Lobo <carlitosg-99@hotmail.com>
2019-10-06 00:11:00 +0200

    Ayuda

--- P1/rtl/processor.vhd
+++ P1/rtl/processor.vhd
@@ -1,4 +1,4 @@
---------------------------------------------------------------------------------
+ --------------------------------------------------------------------------------
 -- Procesador MIPS con pipeline curso Arquitectura 2019-2020
 --
 -- (INCLUIR AQUI LA INFORMACION SOBRE LOS AUTORES)
@@ -29,6 +29,8 @@ architecture rtl of processor is
 
 --General signals
 signal immEx : std_logic_vector(31 downto 0);
+signal PcIn : std_logic_vector(31 downto 0);
+signal PcOut : std_logic_vector(31 downto 0);
 
 -- Control Unit Signals
 signal Branch : std_logic;
@@ -47,6 +49,7 @@ signal Result : std_logic_vector (31 downto 0);
 signal OpA : std_logic_vector (31 downto 0);
 signal OpB : std_logic_vector (31 downto 0);
 signal ALUControl : std_logic_vector (3 downto 0);
+signal Zflag : std_logic;
 
 --Reg-bank signals
 signal Rd2s : std_logic_vector(31 downto 0);
@@ -96,6 +99,16 @@ component alu_control is
    );
 end component;
 
+component alu is
+   port (
+      OpA     : in  std_logic_vector (31 downto 0); -- Operando A
+      OpB     : in  std_logic_vector (31 downto 0); -- Operando B
+      Control : in  std_logic_vector ( 3 downto 0); -- Codigo de control=op. a ejecutar
+      Result  : out std_logic_vector (31 downto 0); -- Resultado
+      ZFlag   : out std_logic                       -- Flag Z
+   );
+end component;
+
 begin
 
    immEx(15 downto 0) <= IDataIn(15 downto 0);
@@ -131,26 +144,71 @@ begin
       ALUControl => ALUControl
       );
 
-   process(Clk)
-      begin
-      case RegDst is
-         when '0' => A3s <= IDataIn(20 downto 16);
-         when '1' => A3s <= IDataIn(15 downto 11);
-         when others => A3s <= (others => '0');
-      end case;
 
-      case MemToReg is
-         when '0' => Wd3s <= Result;
-         when '1' => Wd3s <= DDataIn;
-         when others => A3s <= (others => '0');
-      end case;
+   AL : alu port map (
+      OpB => OpB,
+      OpA => OpA,
+      Control => ALUControl,
+      Result => Result,
+      ZFlag => ZFlag
+      );
+
+process(Clk, Reset)
+   begin
+      if (Reset = '1') then
+         PcOut <= (others => '0');
+      elsif rising_edge(Clk) then 
+      	 PcOut <= PcIn;		     
+      end if;
+
+end process;
+
+process(Clk, Branch, ZFlag)
+begin
+
+   if ((Branch = '1') and (ZFlag = '1')) then
+      PcIn <= PcOut + 4 + (immEx(29 downto 0) & "00");
+   else 
+      PcIn <= PcOut + 4;
+   end if;
+
+end process;
+
+process(Clk, RegDst, IDataIn)
+begin
+   case RegDst is
+      when '0' => A3s <= IDataIn(20 downto 16);
+      when '1' => A3s <= IDataIn(15 downto 11);
+      when others => A3s <= (others => '0');
+   end case;
+
+end process;
+
+process(Clk, MemToReg, Result, DDataIn)
+begin
+   case MemToReg is
+      when '0' => Wd3s <= Result;
+      when '1' => Wd3s <= DDataIn;
+      when others => Wd3s <= (others => '0');
+   end case;
+
+end process;
+
+process(Clk, ALUSrc, Rd2s, immEx)
+begin
 
       case ALUSrc is
          when '0' => OpB <= Rd2s;
          when '1' => OpB <= immEx;
-         when others => A3s <= (others => '0');
+         when others => OpB <= (others => '0');
       end case;
 
-   end process;
+end process;
+
+   DWrEn <= MemWrite;
+   DRdEn <= MemRead;
+   DDataOut <= Rd2s;
+   DAddr <= Result;
+   IAddr <= PcOut;
  
 end architecture;
