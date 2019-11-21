5c6bee75045fe51692d37041bb43fa05a056cd87
Lobo <carlos.gomezlobo99@gmail.com>
2019-11-21 19:03:46 +0100

    Hola

--- P3/ejercicio2.sh
+++ P3/ejercicio2.sh
@@ -4,49 +4,29 @@
 
 # inicializar variables
 p=4
-
 Ninicio=$((2000+512*$p))
 Npaso=64
 Nfinal=$((2000+512*($p+1)))
-TamInicio=1024
-TamFinal=8192
-tam=0
 rep=5
-
-# borrar el fichero DAT y el fichero PNG
-rm -f $fDAT fPNG fDATN
-
-# generar el fichero DAT vacío
-touch $fDAT
+fDAT=./$1/cache.dat
+fPNG=./$1/fallosLectura.png
+fPNG2=./$1/fallosEscritura.png
 
 echo "Running slow and fast..."
-# bucle para N desde P hasta Q
-for ((tam = $TamInicio ; tam <= $TamFinal ; tam*=2;)) do
+rm ./$1/*
+touch $fDAT
 
-  for N in $(seq $Ninicio $(($Npaso*2)) $Nfinal); do
+for N in $(seq $Ninicio $Npaso $Nfinal); do
 
-    N2=$(($N+$Npaso))
+    valgrind --tool=cachegrind --cachegrind-out-file=./$1/slow_out_$N.dat --I1=$1,1,64 --D1=$1,1,64 --LL=8388608,1,64 ./slow $N
+    D1mrSlow=$(cg_annotate ./$1/slow_out_$N.dat | head -18 | tail -1 | awk '{gsub(",","",$5); print $5}')
+    D1mwSlow=$(cg_annotate ./$1/slow_out_$N.dat | head -18 | tail -1 | awk '{gsub(",","",$8); print $8}')
 
-    valgrind --tool=cachegrind --cachegrind-out-file=slow_out_$tam_$N.dat --I1=$tam,1,64 --D1=$tam,1,64 --LL=8388608,1,64 ./slow $N
-    D1mrSlow=cg_annotate slow_out_$tam_$N.dat |head -18 | tail -1 | awk '{print $5}'
-    D1mwSlow=cg_annotate slow_out_$tam_$N.dat |head -18 | tail -1 | awk '{print $8}'
+    valgrind --tool=cachegrind --cachegrind-out-file=./$1/fast_out_$N.dat --I1=$1,1,64 --D1=$1,1,64 --LL=8388608,1,64 ./fast $N
+    D1mrFast=$(cg_annotate ./$1/fast_out_$N.dat | head -18 | tail -1 | awk '{gsub(",","",$5); print $5}')
+    D1mwFast=$(cg_annotate ./$1/fast_out_$N.dat | head -18 | tail -1 | awk '{gsub(",","",$8); print $8}')
 
-    valgrind --tool=cachegrind --cachegrind-out-file=slow_out_$tam_$N2.dat --I1=$tam,1,64 --D1=$tam,1,64 --LL=8388608,1,64 ./slow $N2
-    D1mrSlow2=cg_annotate slow_out_$tam_$N2.dat |head -18 | tail -1 | awk '{print $5}'
-    D1mwSlow2=cg_annotate slow_out_$tam_$N2.dat |head -18 | tail -1 | awk '{print $8}'
-
-    valgrind --tool=cachegrind --cachegrind-out-file=fast_out_$tam_$N.dat --I1=$tam,1,64 --D1=$tam,1,64 --LL=8388608,1,64 ./fast $N
-    D1mrFast=cg_annotate fast_out_$tam_$N.dat |head -18 | tail -1 | awk '{print $5}'
-    D1mwFast=cg_annotate fast_out_$tam_$N.dat |head -18 | tail -1 | awk '{print $8}'
-
-    valgrind --tool=cachegrind --cachegrind-out-file=fast_out_$tam_$N2.dat --I1=$tam,1,64 --D1=$tam,1,64 --LL=8388608,1,64 ./fast $N2
-    D1mrFast2=cg_annotate fast_out_$tam_$N2.dat |head -18 | tail -1 | awk '{print $5}'
-    D1mwFast2=cg_annotate fast_out_$tam_$N2.dat |head -18 | tail -1 | awk '{print $8}'
-
-    echo "$N	$D1mrSlow	$D1mwSlow $D1mrFast $D1mwFast" >> $cache_$tam.dat
-    echo "$N2	$D1mrSlow2	$D1mwSlow2 $D1mrFast2 $D1mwFast2" >> $fDAT
-  done
-
+    echo "$N    $D1mrSlow   $D1mwSlow   $D1mrFast   $D1mwFast" >> $fDAT
 done
 
 echo "Generating plot..."
@@ -54,15 +34,24 @@ echo "Generating plot..."
 # estándar el script que está entre "<< END_GNUPLOT" y "END_GNUPLOT"
 export PATH=$PATH:/share/apps/tools/gnuplot/bin
 gnuplot << END_GNUPLOT
-set title "Slow-Fast Execution Time"
-set ylabel "Execution time (s)"
+set title "Fallos de Lectura"
+set ylabel "Numero de Fallos"
 set xlabel "Matrix Size"
 set key right bottom
 set grid
 set term png
 set output "$fPNG"
 plot "$fDAT" using 1:2 with lines lw 2 title "slow", \
-     "$fDAT" using 1:3 with lines lw 2 title "fast"
+"$fDAT" using 1:4 with lines lw 2 title "fast"
 replot
+set title "Fallos de Escritura"
+set ylabel "Numero de Fallos"
+set xlabel "Matrix Size"
+set key right bottom
+set grid
+set term png
+set output "$fPNG2"
+plot "$fDAT" using 1:3 with lines lw 2 title "slow", \
+"$fDAT" using 1:5 with lines lw 2 title "fast"
 quit
 END_GNUPLOT
