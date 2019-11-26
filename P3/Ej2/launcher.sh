912733342fff1406204ff837b4eb34699ca60988
Lardis <lardiesrodrigo@gmail.com>
2019-11-26 12:41:22 +0100

    resultados del 3 y poco mas

--- P3/Ej2/launcher.sh
+++ P3/Ej2/launcher.sh
@@ -28,8 +28,8 @@ set term png
 set output "$fPNGR"
 plot "1024/$fDAT" using 1:2 with lines lw 2 title "slow\\\\_1024", \
 "1024/$fDAT" using 1:4 with lines lw 2 title "fast\\\\_1024", \
-"2048/$fDAT" using 1:2 with lines lw 2 title "slow\\\\_2038", \
-"2048/$fDAT" using 1:4 with lines lw 2 title "fast\\\\_2038", \
+"2048/$fDAT" using 1:2 with lines lw 2 title "slow\\\\_2048", \
+"2048/$fDAT" using 1:4 with lines lw 2 title "fast\\\\_2048", \
 "4096/$fDAT" using 1:2 with lines lw 2 title "slow\\\\_4096", \
 "4096/$fDAT" using 1:4 with lines lw 2 title "fast\\\\_4096", \
 "8192/$fDAT" using 1:2 with lines lw 2 title "slow\\\\_8192", \
@@ -44,8 +44,8 @@ set term png
 set output "$fPNGW"
 plot "1024/$fDAT" using 1:3 with lines lw 2 title "slow\\\\_1024", \
 "1024/$fDAT" using 1:5 with lines lw 2 title "fast\\\\_1024", \
-"2048/$fDAT" using 1:3 with lines lw 2 title "slow\\\\_2038", \
-"2048/$fDAT" using 1:5 with lines lw 2 title "fast\\\\_2038", \
+"2048/$fDAT" using 1:3 with lines lw 2 title "slow\\\\_2048", \
+"2048/$fDAT" using 1:5 with lines lw 2 title "fast\\\\_2048", \
 "4096/$fDAT" using 1:3 with lines lw 2 title "slow\\\\_4096", \
 "4096/$fDAT" using 1:5 with lines lw 2 title "fast\\\\_4096", \
 "8192/$fDAT" using 1:3 with lines lw 2 title "slow\\\\_8192", \
